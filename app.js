e080f28091567a705a76aeed011bca0a82a11e3b
Keith Musig <musigman@gmail.com>
2020-07-23 21:22:53 -0700

    updated JS

--- app.js
+++ app.js
@@ -1,19 +1,42 @@
-function getdogname(){
-    var userName = prompt('What is your Dog`s name?');
-    return userName;
+
+ function getuserName(){
+     var userNameInFunction = prompt('What is the name of your Dog?');
+     alert('Hello There... ' + userNameInFunction);
+     document.write('<h2>' + userNameInFunction + '<h2>is a good dog!</h2>');
+     return userNameInFunction;
 }
-var userName = getdogname();
+ var userName = getuserName();
 
-alert('Hello There...' + userName);
-
-
-alert('Good Dog, ' + userName);
-
-while(userName2 == ""){
-var userName2 = getdogname();
-
-
+ function confirmAskQuestions(){
+    confirm(userName + ', I have some questions.');
 }
-document.write('<h2>' + userName + ' is a good dog!</h2>');
+ confirmAskQuestions();
 
-//document.write('<h2>' + userName + '</h2>');
+ function askQuestion(){
+     var userCodeAnswer = prompt('Do you enjoy the Dog park? (yes or no)');
+     if (userCodeAnswer == "yes"){
+         alert('I do too');
+     } else {
+         alert('That is too bad.  It is so much fun');
+    }
+ }
+ askQuestion();
+
+
+ function askTime(){
+     var userTimeInFunction = prompt('What hour is it in military time 0-24?');
+     return userTimeInFunction;
+ }
+ var userTime = askTime();
+
+
+ function checkTime(){
+     if(userTime < 12){
+         alert('Good Morning, What a smart dog!');
+     } else if (userTime >= 12 && userTime < 18){
+         alert('Good Afternoon, You are a smart dog!');
+     } else {
+        alert('Good Evening,You are a smart dog!');
+    }
+}
+checkTime();
